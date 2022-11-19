9887e0f4ae18245a5a1e1f2a690394d25a82c992
Daniel PC <daniel.barczynski@onet.pl>
2022-11-19 16:14:43 +0100

    update

--- src/App.js
+++ src/App.js
@@ -2,20 +2,35 @@ import { useState, useRef, useEffect } from 'react';
 import TodoList from './TodoList'
 import { v4 as uuidv4 } from 'uuid';
 
+const LOCAL_STORAGE_KEY = 'todosKey.todos';
+
 function App() {
   const [todos, setTodos] = useState([])
   const refTodo = useRef();
 
   useEffect(() => {
-    
-  })
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    console.log(storedTodos);
+    if (storedTodos) {
+      setTodos(storedTodos);
+      console.log('stored');
+    }
+  }, []) // calling it once, while our component (empty array) loads. if state of empty array occurs, use this function 
+
+  useEffect(() => {
+    const saveTodo = localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+    console.log(saveTodo);
+  }, [todos]) // calling it when there are some todos in our array
 
   function addTodo(e) {
     const name = refTodo.current.value;
+    if (!name) return;
+  
     setTodos(prev => {
       return [...prev, {id: uuidv4(), name: name, complete: false}]
     })
     refTodo.current.value = null;
+  
   }
 
   return (
--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+    // removed strictmode because react is calling twice useEffect
     <App />
-  </React.StrictMode>
 );
